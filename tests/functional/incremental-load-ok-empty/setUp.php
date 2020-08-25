29ae8c7dbe769bc2163de33e060e5b2e2e19a913
Michal Jurečko <michal.jurecko@keboola.com>
2020-08-25 09:44:53 +0200

    Datadir tests for incremental load

---
+++ tests/functional/incremental-load-ok-empty/setUp.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Keboola\DbExtractor\FunctionalTests\DatadirTest;
+use Keboola\DbExtractor\FunctionalTests\DatabaseSetupManager;
+
+return function (DatadirTest $test): void {
+    $manager = new DatabaseSetupManager($test->getConnection());
+    $manager->createIncrementalTable();
+    // no rows
+};
