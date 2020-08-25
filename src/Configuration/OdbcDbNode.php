e5f1602bac5027d1aab644694f354f5db0bf33aa
Michal Jurečko <michal.jurecko@keboola.com>
2020-08-25 11:07:09 +0200

    Set required db properties + test

--- src/Configuration/OdbcDbNode.php
+++ src/Configuration/OdbcDbNode.php
@@ -21,4 +21,19 @@ class OdbcDbNode extends DbNode
         // https://www.ibm.com/support/knowledgecenter/en/SSGU8G_12.1.0/com.ibm.adref.doc/ids_adr_0045.htm
         $builder->scalarNode('serverName')->isRequired();
     }
+
+    protected function addHostNode(NodeBuilder $builder): void
+    {
+        $builder->scalarNode('host')->isRequired();
+    }
+
+    protected function addDatabaseNode(NodeBuilder $builder): void
+    {
+        $builder->scalarNode('database')->cannotBeEmpty()->isRequired();
+    }
+
+    protected function addPortNode(NodeBuilder $builder): void
+    {
+        $builder->scalarNode('port')->isRequired();
+    }
 }
